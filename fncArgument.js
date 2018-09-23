9aac7840c71d3b5d85b2746b64ca10339fe4e01e
chibuike vincent <chibuikepatrick2@gmail.com>
2018-09-23 11:19:32 +0100

    day 10 of 100DaysOfCode, use of function to generate lat and long

---
+++ fncArgument.js
@@ -0,0 +1,17 @@
+
+// Day 8
+// let butn = document.getElementById('btn').addEventListener('click', function ( ) {
+//   let input =  document.getElementById('input')
+//   let result = input.value * 0.0001
+//   console.log(result + '%')
+// })
+
+// let input = document.getElementById('input')
+// console.log(input.value * 3)
+
+let butn = document.getElementById('btn').addEventListener('click', function () {
+  let playername = document.getElementById('user').innerHTML
+  let input =  document.getElementById('input')
+   playername
+  console.log( '%')
+})
